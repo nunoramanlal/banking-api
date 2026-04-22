42baa63d2b3cbb5c07ab0104e7af4ef7df0dc35c
nunoramanlal <nunomanoje@hotmail.com>
2026-04-22 23:28:35 +0100

    fix: Correctly using application-test.yaml

--- src/test/java/com/eaglebank/banking_api/it/UserControllerIT.java
+++ src/test/java/com/eaglebank/banking_api/it/UserControllerIT.java
@@ -19,10 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerIT {
 
     @Autowired
